3d0d3758876ce3f9a2d1cb7b8e79675ae918d00c
AnorX_zq13@student.itstep.org <AnorX_zq13@student.itstep.org>
2023-09-27 17:24:31 +0300

    Добавьте файлы проекта.

---
+++ Laboratory4/MainWindow.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Laboratory4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            button1.Content = "Button 1";
+            button2.Content = "Button 2";
+            button3.Content = "Button 3";
+            button4.Content = "Button 4";
+            button5.Content = "Button 5";
+        }
+    }
+}
